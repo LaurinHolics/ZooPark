37d785d4e6286577924b0bacdcca9fed0197608d
Laurin <holics.laurin@yahoo.com>
2021-09-20 15:24:24 +0300

    add gettAllElepfants

--- src/ro/itschool/curs/Main.java
+++ src/ro/itschool/curs/Main.java
@@ -1,13 +1,15 @@
 package ro.itschool.curs;
 
+import java.sql.SQLException;
+
 import ro.itschool.curs.dao.ElephantDao;
 
 public class Main {
 
-	public static void main(String[] args) {
-		ElephantDao dumbo = new ElephantDao();
-		System.out.println(dumbo.getElephantById(1));
-
+	public static void main(String[] args) throws SQLException {
+		ElephantDao elephantDao = new ElephantDao();
+//		System.out.println(elephantDao.getElephantById(1));
+		System.out.println(elephantDao.getAllElephants());
 	}
 
 }
